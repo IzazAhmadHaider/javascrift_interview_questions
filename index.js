4f8b28096570b957017b8e2cb4da66335b298021
izaz ahmad <izazahmadhaider0@gmail.com>
2024-06-27 10:41:39 +0500

    Added UI changes

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var buttonHtml = '<button class="global-button" onclick="location.href=\'/index.html\'">Home</button>';
-    var buttonContainer = document.createElement('div');
-    buttonContainer.innerHTML = buttonHtml;
-    document.body.appendChild(buttonContainer.firstChild);
-
-    // Apply styles for the button
-    var buttonStyle = buttonContainer.firstChild.style;
-    buttonStyle.position = 'fixed';
-    buttonStyle.bottom = '20px'; // Adjust bottom position as needed
-    buttonStyle.right = '20px'; // Adjust right position as needed
-    buttonStyle.zIndex = '1000'; // Ensure it's above other content
-
-    // CSS for the button
-    buttonStyle.fontFamily = "'Saira Condensed', sans-serif";
-    buttonStyle.fontWeight = '800';
-    buttonStyle.fontSize = '16px';
-    buttonStyle.backgroundColor = '#4CAF50';
-    buttonStyle.color = 'white';
-    buttonStyle.border = 'none';
-    buttonStyle.padding = '10px 20px';
-    buttonStyle.borderRadius = '5px';
-    buttonStyle.cursor = 'pointer';
-});
+document.addEventListener("DOMContentLoaded", function () {
+    var buttonHtml2 = 
+      '<button class="button-64" role="button" onclick="location.href=\'/index.html\'"><span class="text">Go To Home Page</span></button>';
+    var buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("home_navigator_button");
+    buttonContainer.innerHTML = buttonHtml2
+    document.body.appendChild(buttonContainer);
+  
+    // Add the favicon
+    var link = document.createElement("link");
+    link.rel = "icon";
+    link.href = "/images/js.svg";
+    document.head.appendChild(link);
+  });
+  
