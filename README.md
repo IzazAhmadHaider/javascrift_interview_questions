e8fab90e13d90e20572bbfcda9a0e32b62e11001
izaz ahmad <izazahmadhaider0@gmail.com>
2024-07-03 17:04:51 +0500

    added changes For question no 1

--- README.md
+++ README.md
@@ -2,24 +2,3 @@
 
 A curated collection of JavaScript interview questions and answers to help developers prepare for technical interviews. This repo covers basic to advanced topics, including ES6 features, async programming, and more. Each question includes detailed explanations. Contributions are welcome!
 
-![Color Plates](images/color-plates.png)
-
-## Contribution Guidelines
-
-When contributing to this repository, please adhere to the following guidelines:
-
-- **Use only the colors provided in the above image** for any visual elements.
-- Ensure your contributions are clear and concise.
-- Provide detailed explanations for each question and answer.
-- Format your code properly and check for errors before submitting.
-- Use only font-faimly Saira-Condenesed.
-
-Thank you for your contributions!
-
-## Topics Covered
-
-- Basic JavaScript Concepts
-- ES6 Features
-- Asynchronous Programming
-- Advanced JavaScript Topics
-- And more...
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const numbers = [2, 4, 3, 5, 7, 8, 1, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 4];
+/*
+Interview Question No. 1:
+
+Given an array and a target value, find all pairs of values from the array whose sum equals the target value. Ensure that there are no duplicate pairs. For example, [[1, 2], [1, 2], [2, 1]] should be considered as a single pair. Additionally, there should be no self-addition unless the array contains multiple instances of the same number. For instance, if the target value is 6 and the array contains only one "3", then [3, 3] is invalid. However, if the array contains multiple "3"s, then [3, 3] is valid.
+
+ */
+
+const numbers = [2, 4, 3, 5, 7, 8, 1, 4, 5, 6, 7, 8, 9, 1, 2, 4, 4];
 const targetSum = 8;
 const result = findPairs(numbers, targetSum);
 console.log(result);
@@ -28,4 +35,8 @@ function findPairs(arr, total) {
 
   return result;
 }
+
+// ----------------------------------------------------------------//
+
+
 
