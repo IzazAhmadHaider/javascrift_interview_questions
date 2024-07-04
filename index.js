876363f1c12edeba2527c4c5e8b7e26fee369f11
izaz ahmad <izazahmadhaider0@gmail.com>
2024-07-04 17:29:50 +0500

    changed added new problem

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ Given an array and a target value, find all pairs of values from the array whose
 const numbers = [2, 4, 3, 5, 7, 8, 1, 4, 5, 6, 7, 8, 9, 1, 2, 4, 4];
 const targetSum = 8;
 const result = findPairs(numbers, targetSum);
-console.log(result);
+//For Answer Uncomment the below ↓ line
+// console.log(result);
 
 function findPairs(arr, total) {
   let result = [];
@@ -20,9 +21,8 @@ function findPairs(arr, total) {
     let complement = total - current;
 
     if (seen.has(complement)) {
-      let pair = [current, complement].sort(); 
+      let pair = [current, complement].sort();
       let pairKey = pair.toString();
-      console.log(pairKey);
 
       if (!seenPairs.has(pairKey)) {
         result.push(pair);
@@ -40,3 +40,36 @@ function findPairs(arr, total) {
 
 
 
+
+/* Leet Code Problem twoSum
+
+we have an array of number and target value both in integers we have to find indexes of First pairs which sum equals to target value but result index pairs will have no common numbers like 3,3 if target is 6. 
+
+*/
+
+const nums = [2, 7, 8, 1, 11, 15];
+const target = 9;
+
+var twoSum = function (nums, target) {
+
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+
+    const complement = target - element;
+
+    const secvalindex = nums.findIndex(
+      (el, ind) => el === complement && ind !== i
+    );
+
+    if (secvalindex !== -1) {
+      return [i, secvalindex];
+    }
+  }
+  return [];
+};
+console.log(twoSum(nums, target));
+
+
+
+
+// ----------------------------------------------------------------//
